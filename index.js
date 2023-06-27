0bf005d7a25704be64a80bf317be740f37acbc1a
Katie Sayers <kate@student.dev-island.org>
2023-06-27 13:54:25 -1000

    got mouseover event working

--- index.js
+++ index.js
@@ -1,29 +1,33 @@
+let sketchPad = document.querySelector(".sketch-pad");
+let gridSize = document.querySelector("#js-grid-size");
+let apply = document.querySelector("#js-apply");
 
-let sketchPad = document.querySelector('.sketch-pad');
-let gridSize = document.querySelector('#grid-size');
-let apply = document.querySelector('.apply');
-let rows = 16; 
-let columns = 16; 
+const testFun = (event) => {
+	event.target.classList.add('active')
+}
 
 const createGrid = () => {
-    rows = gridSize; 
-    columns = gridSize
-    //create column in the number of given columns
-for (let i = 0; i < columns; i++) {
-    let column = document.createElement('div'); 
-    column.className = 'column';
+  let rows = gridSize.value;
+  let columns = gridSize.value;
+
+	//reset grid when apply is clicked
+	sketchPad.innerHTML = ''
+
+  //create column in the number of given columns
+  for (let i = 0; i < columns; i++) {
+    let column = document.createElement("div");
+    column.className = "column";
     //create row in the number of given rows
     for (let j = 0; j < rows; j++) {
-        let row = document.createElement('div');
-        row.className = 'row';
-        column.appendChild(row)
+      let row = document.createElement("div");
+      row.className = "row";
+			row.addEventListener('mouseover', testFun)
+      column.appendChild(row);
     }
-    sketchPad.append(column)
-}
-}
+    sketchPad.append(column);
+  }
+	// sketchPad.addEventListener('mouseover', testFun)
+};
 
-// createGrid();
-
-apply.addEventListener('click', createGrid);
-
+apply.addEventListener('click', createGrid)
 
